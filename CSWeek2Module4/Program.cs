8ed7239b74d58c2944e8a7833faae9298894a084
Esteban Salomon <esalomon6492@southwestuniversity.edu>
2021-05-23 21:29:00 -0600

    CS Week 2 Module 4

---
+++ CSWeek2Module4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSWeek2Module4
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime date1 = new DateTime(2021,01,01);
+            DateTime date2 = new DateTime(2021,12,31);
+
+            if (date1 < date2)
+            {
+                Console.WriteLine("Date 2 is grater");
+            }
+            else
+            {
+                Console.WriteLine("Date 1 is grater");
+            }
+        }
+    }
+}
